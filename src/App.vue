54e7cc27f4dbe6029507f7d17419fa950c049f69
jadeleafmoon <jadeleafmoon@gmail.com>
2023-09-06 15:19:05 -0400

    feat: add BlogPost component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to the blog!</h1>
+  <BlogPost />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogPost from "./components/BlogPost.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { BlogPost }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post">
+    <h1>Blog Post 1</h1>
+    <p>Some text 1 ...</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+.post {
+  background-color: lightcoral;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+}
+</style>
